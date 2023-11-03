30a5d3866f2de55161b86939b9e5bb4032dbd258
RememberIOm <rememberiom@icloud.com>
2023-11-03 09:52:18 +0900

    9주차 과제 파일 수정

--- Embedded_Software/week9.c
+++ Embedded_Software/week9.c
@@ -5,22 +5,24 @@
 int main() {
 	unsigned char value = 0;
 	int music[] = {3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 2, 0, 3, 5, 5, 0,
-	3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 3, 2, 1, 0, 0, 0};
+					3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 3, 2, 1, 0, 0, 0};
+					
 	DDRA= 0xff;
 	
 	for (;;) {
 		for (int i = 0; i < 32; ++i) {
 			PORTA = value;
-			_delay_ms(200);
+			
+			_delay_ms(150);
 			
 			if (music[i] == 0) {
 				// pass
-				} else {
+			} else {
 				value = 1 << (8 - music[i]);
 				PORTA = 0;
 			}
 			
-			_delay_ms(200);
+			_delay_ms(150);
 		}
 	}
 }
