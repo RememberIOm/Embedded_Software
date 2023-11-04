bc4832079767f6da4f36a2070caf34d787176ccf
RememberIOm <rememberiom@icloud.com>
2023-11-04 16:09:30 +0900

    10주차 1번 파일 수정

--- Embedded_Software/week10/week10_1.c
+++ Embedded_Software/week10/week10_1.c
@@ -8,9 +8,16 @@ const uc digit[10] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27, 0x7f, 0x6f
 const uc fnd_sel[4] = {0x08, 0x04, 0x02, 0x01};
 const uc dot = 0x80;
 
-void printNum(const uc const num[]) {
+void printNum(const int* const num) {
+	const uc fnd[] = {
+		digit[*num / 1000],
+		digit[*num % 1000 / 100],
+		digit[*num % 100 / 10] + dot,
+		digit[*num % 10]
+	};
+
 	for (int i = 0; i < 4; ++i) {
-		PORTC = num[i];
+		PORTC = fnd[i];
 		PORTG = fnd_sel[i];
 		_delay_us(250);
 	}
@@ -20,16 +27,11 @@ int main() {
 	DDRC = 0xff;
 	DDRG = 0x0f;
 	
-	uc num[4];
+	int num = 0;
 	
 	while (1) {
-		for (int i = 0; i < 10000; ++i) {
-			num[0] = digit[i / 1000];
-			num[1] = digit[i % 1000 / 100] + dot;
-			num[2] = digit[i % 100 / 10];
-			num[3] = digit[i % 10];
+		num = (num + 1) % 10000;
 			
-			printNum(num);
-		}
+		printNum(&num);
 	}
 }
