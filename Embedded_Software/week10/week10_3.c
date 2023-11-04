96f20e67a83f1c80991fd79f5a017b8f3d6f1e26
RememberIOm <rememberiom@icloud.com>
2023-11-04 16:02:09 +0900

    10주차 3번 파일 수정

--- Embedded_Software/week10/week10_3.c
+++ Embedded_Software/week10/week10_3.c
@@ -1,7 +1,6 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdlib.h>
 #include <util/delay.h>
 
 #define START 1
@@ -13,29 +12,25 @@ const uc digit[10] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27, 0x7f, 0x6f
 const uc fnd_sel[4] = {0x08, 0x04, 0x02, 0x01};
 const uc dot = 0x80;
 
-int count = 0, signal = 0;
+int count = 0, signal = STOP;
 
 void display_fnd(const int* const count) {
-	int fnd[4];
-	
-	fnd[3] = (*count / 1000) % 10;
-	fnd[2] = (*count / 100) % 10;
-	fnd[1] = (*count / 10) % 10;
-	fnd[0] = *count % 10;
+	const uc fnd[] = {
+		digit[*count / 1000],
+		digit[*count % 1000 / 100],
+		digit[*count % 100 / 10] + dot,
+		digit[*count % 10]
+	};
 
 	for (int i = 0; i < 4; ++i) {
-		PORTC = digit[fnd[i]];
+		PORTC = fnd[i];
 		PORTG = fnd_sel[i];
-		_delay_ms(2);
+		_delay_us(250);
 	}
 }
 
 ISR(INT4_vect) {
-	if (signal == START) {
-		signal = STOP;
-	} else {
-		signal = START;
-	}
+	signal ^= 1;
 }
 
 ISR(INT5_vect) {
@@ -54,5 +49,9 @@ int main() {
 	
 	while (1) {
 		display_fnd(&count);
+		
+		if (signal == START) {
+			count = (count + 1) % 10000;
+		}
 	}
 }
