a04947080cc367a10fafd980d95010d326016576
RememberIOm <rememberiom@icloud.com>
2023-11-08 01:25:01 +0900

    9주차 파일 수정 (알고리즘 및 코드 포맷 수정)

--- Embedded_Software/week9.c
+++ Embedded_Software/week9.c
@@ -3,26 +3,32 @@
 #include <util/delay.h>
 
 int main() {
-	unsigned char value = 0;
-	int music[] = {3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 2, 0, 3, 5, 5, 0,
-					3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 3, 2, 1, 0, 0, 0};
+	const int music[] = {
+    3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 2, 0, 3, 5, 5, 0,
+    3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 3, 2, 1, 0, 0, 0
+    };
 					
-	DDRA= 0xff;
+	const int music_size = sizeof(music) / sizeof(int);
+					
+	DDRA = 0xff;
+	
+	unsigned char value = 0;
 	
 	for (;;) {
-		for (int i = 0; i < 32; ++i) {
+		for (int i = 0; i < music_size; ++i) {
+			if (music[i] != 0) {
+				value = 1 << (8 - music[i]);
+			}
+			
 			PORTA = value;
 			
-			_delay_ms(150);
+			_delay_ms(200);
 			
-			if (music[i] == 0) {
-				// pass
-			} else {
-				value = 1 << (8 - music[i]);
+			if (music[(i + 1) % music_size] != 0) {
 				PORTA = 0;
 			}
 			
-			_delay_ms(150);
+			_delay_ms(100);
 		}
 	}
 }
