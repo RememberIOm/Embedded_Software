445b2754bb8982d82991d9d945771c6dad2fae50
RememberIOm <rememberiom@icloud.com>
2023-11-12 17:19:40 +0900

    11주차 2번 파일 수정 (Cygnus Garden)

--- Embedded_Software/week11/week11_2.c
+++ Embedded_Software/week11/week11_2.c
@@ -7,24 +7,44 @@
 #define OFF 0
 
 enum scale_timer {
-  DO = 17,
-  RE = 43,
+  LTI = 3,
+  DO = 17, DOS = 30,
+  RE = 43, RES = 55,
   MI = 66,
-  FA = 77,
-  SOL = 97,
-  LA = 114,
+  FA = 77, FAS = 87,
+  SOL = 97, SOLS = 105,
+  LA = 114, LAS = 122,
   TI = 129,
-  HDO = 137
+  HDO = 137, HDOS = 143,
+  HRE = 150, HRES = 156,
+  HMI = 161,
+  HFA = 166, HFAS = 171,
+  HSOL = 176, HSOLS = 181,
+  HLA = 185, HLAS = 189,
+  HTI = 193,
+  HHDO = 196
 };
 
-const int scale[] = {0, DO, RE, MI, FA, SOL, LA, TI, HDO};
+const int scale[] = {
+  0, 
+  LTI,
+  DO, DOS, RE, RES, MI, FA, FAS, SOL, SOLS, LA, LAS, TI,
+  HDO, HDOS, HRE, HRES, HMI, HFA, HFAS, HSOL, HSOLS, HLA, HLAS, HTI,
+  HHDO
+};
 int state = ON;
 int value = 0;
 int timer_state = ON;
 
 const int music[] = {
-  3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 2, 0, 3, 5, 5, 0,
-  3, 2, 1, 2, 3, 3, 3, 0, 2, 2, 3, 2, 1, 0, 0, 0
+  11, 0, 13, 0, 15, 0, 8, 0, 0, 0, 0, 0, 8, 15, 13, 11, 10, 8, 10, 0, 0, 0, 0, 0,
+  6, 0, 0, 0, 11, 0, 8, 0, 0, 0, 3, 1, 3, 0, 0, 0, 6, 0, 3, 0, 0, 0, 0, 0,
+  1, 0, 3, 0, 6, 0, 8, 0, 0, 0, 0, 0, 8, 15, 13, 11, 10, 8, 10, 0, 0, 0, 0, 0,
+  6, 8, 10, 11, 10, 0, 8, 0, 3, 0, 0, 1, 3, 0, 0, 0, 6, 0, 3, 0, 0, 0, 0, 0,
+  0, 0, 0, 6, 8, 15, 20, 0, 0, 0, 0, 0, 13, 0, 0, 0, 13, 11, 13, 0, 0, 0, 0, 0,
+  18, 0, 0, 0, 16, 0, 15, 0, 15, 16, 15, 0, 0, 0, 13, 15, 13, 0, 11, 0, 10, 0, 13, 0,
+  11, 0, 0, 0, 10, 0, 8, 0, 0, 0, 0, 0, 4, 0, 0, 6, 8, 13, 10, 0, 0, 0, 0, 0,
+  6, 0, 0, 0, 3, 0, 8, 0, 15, 16, 15, 0, 0, 0, 13, 15, 13, 0, 11, 0, 13, 0, 10, 0
 };
 const int music_size = sizeof(music) / sizeof(int);
 
@@ -39,12 +59,12 @@ ISR(TIMER0_OVF_vect) {
     }
   
     TCNT0 = value;
-  }  
+  }
 }
 
 int main() {
-  DDRB = 0x10; // ?? ??
-  TCCR0 = 0x03; // 32??
+  DDRB = 0x10;
+  TCCR0 = 0x05;
   TIMSK = 0x01;
   sei();
   
@@ -56,13 +76,13 @@ int main() {
       
       timer_state = ON;
       
-      _delay_ms(200);
+      _delay_ms(150);
       
       if (music[(i + 1) % music_size] != 0) {
         timer_state = OFF;
       }
       
-      _delay_ms(100);
+      _delay_ms(50);
     }
   }
 }
